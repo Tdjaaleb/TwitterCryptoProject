10640392cfb1d62b226db8aa1da23496488d3df6
Tdjaaleb <82368417+Tdjaaleb@users.noreply.github.com>
2022-01-03 14:55:11 +0100

    Create cryptoDict.py

---
+++ cryptoDict.py
@@ -0,0 +1,10 @@
+CryptoDict = {
+    "BTC" : ["btc","bitcoin"],
+    "ETH" : ["eth"],
+    "AVAX" : ["avalanche", "avax"],
+    "DOGE" : ["doge"],
+    "BNB" : ["binance coin","bnb"],
+    "DOT" : ["dot"],
+    "XRP" : ["xrp","ripple"],
+    "LUNA" : ["luna","terra"]
+}
