9933b207fa73f4867c2f6fc82ee9a0171be2a757
SoleneVac <97027738+SoleneVac@users.noreply.github.com>
2022-01-03 16:49:00 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -4,3 +4,13 @@ class Params:
         self.symbol = symbol,
         self.currency = currency,
         self.interval = interval
+
+class Tweet : 
+    def __init__(self,ID, author , text , date , symbol, answeringTo=[]) : 
+        self.id = ID
+        self.author = author
+        self.text = text 
+        self.date = date 
+        self.symbol = symbol.upper() + 'USDT'
+        self.answeringTo = answeringTo 
+        
