de255416c30a8ff339a6925ebb7248ad92958d2c
Tdjaaleb <82368417+Tdjaaleb@users.noreply.github.com>
2022-01-03 14:50:06 +0100

    Update func.py

--- func.py
+++ func.py
@@ -1,1 +1,28 @@
+import tweepy
+from datetime import datetime
 
+def Get_All_Tweets(Username):
+  user = client_twi.get_user(
+    username = Username,
+    user_field = 'public_metrics'
+  ) #Récupère les informations de l'utilisateur en fonction de son nom d'utilisateur
+  
+  user_id = user.data["id"] #Id utilisateur
+  iter_max = int(user.data.public_metrics["tweet_count"]/100)+1 #Nb de tweets divisé par 100 pour créer la boucle
+  
+  timer = str(datetime.utcnow())
+  timer = timer.replace(' ','T')
+  timer = timer.split('.')[0]+'Z'
+  
+  for i in range(iter_max):
+    Tweets = client_twi.get_users_tweets(
+        id=user_id,
+        max_results=100,
+        tweet_fields='created_at'
+        end_time=timer
+    )
+    for j in enumerate(Tweets.data):
+      j[1].text.lower().find()
+  
+  
+  
