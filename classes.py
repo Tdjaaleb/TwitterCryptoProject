5517ef3ad15329d39452dd0aac408bd3ae4fdec9
SoleneVac <97027738+SoleneVac@users.noreply.github.com>
2022-01-03 16:50:56 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -6,11 +6,12 @@ class Params:
         self.interval = interval
 
 class Tweet : 
-    def __init__(self,ID, author , text , date , symbol, answeringTo=[]) : 
+    def __init__(self,ID, author , text , date , answeringTo=[]) : 
         self.id = ID
         self.author = author
         self.text = text 
         self.date = date 
-        self.symbol = symbol.upper() + 'USDT'
         self.answeringTo = answeringTo 
+        
+    def draw(self) : 
         
