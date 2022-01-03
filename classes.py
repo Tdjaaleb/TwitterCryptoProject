ee5abfa6f1c7bd110cb7b440184d63a725c1336c
Tdjaaleb <82368417+Tdjaaleb@users.noreply.github.com>
2022-01-03 16:30:37 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,1 +1,6 @@
-
+class Params:
+    def __init__(self, username, symbol, currency, interval):
+        self.user = username,
+        self.symbol = symbol,
+        self.currency = currency,
+        self.interval = interval
