ca21d2f8463dd66606641717e4e3dee66c075713
Tdjaaleb <82368417+Tdjaaleb@users.noreply.github.com>
2022-01-04 12:27:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from datetime import datetime
-
-import tweepy
 from binance.client import Client
-from mplfinance.original_flavor import candlestick_ohlc
-
-from API_KEY import key,secret,access,access_s,bearer
 from func import Get_Relevant_Tweets, Plot_Historical
-from cryptoDict import CryptoDict
-from classes import Params
 
 client_bi = Client()
-
-client_twi = tweepy.Client(
-    bearer_token = bearer,
-    consumer_key = key,
-    consumer_secret = secret,
-    access_token = access,
-    access_token_secret = access_s
-)
 
 Username = input("Veuillez entrer l'identifiant Twitter de l'utilisateur : ")
 Symbol = input("Quelle cryptomonnaie souhaitez vous analyser ? ") #BTC / ETH / AVAX / BNB / DOGE / ...
 Currency = input("En quelle monnaie voulez-vous analyser les cours ? ") #USDT / EUR / GBP
 Interval = input("Sur combien de temps souhaitez vous analyser ? ") #1 Heure / 24 Heures
 
-params = Params(Username, Symbol, Currency, Interval)
-
-Liste_Tweets = Get_Relevant_Tweets(params.user)
+Liste_Tweets = Get_Relevant_Tweets(Username, Symbol)
 
 AllTime_Klines = client_bi.get_klines(
-    symbol = params.symbol+params.currency,
+    symbol = Symbol+Currency,
     interval = Client.KLINE_INTERVAL_1DAY,
     limit = 1000
 )
