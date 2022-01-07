8615d9c7a1402b5da600fb9d541ff2c9af826786
Tdjaaleb <82368417+Tdjaaleb@users.noreply.github.com>
2022-01-07 16:53:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+#Import des fonctions
 from func import Get_Relevant_Tweets, Plot_MostImpactfulTweets
 
+#Demande à l'utilisateur de rentrer un "@" twitter
 Username = input("Veuillez entrer l'identifiant Twitter de l'utilisateur : ")
 
+#Demande à l'utilisateur de choisir un cryptomonnaie
 Symbol = input("Quelle cryptomonnaie souhaitez vous analyser ? (BTC/ETH/AVAX/DOGE/BNB)")
 while Symbol!="BTC" and Symbol!="ETH" and Symbol!="AVAX" and Symbol!="DOGE" and Symbol!="BNB":
     Symbol = input("Veuillez entrer une cryptomonnaie disponible pour analyse (BTC, ETH, AVAX, DOGE, BNB) :")
 
+#Demande à l'utilisateur de choisir une monnaie
 Currency = input("En quelle monnaie voulez-vous analyser les cours ? (EUR/USDT/GBP")
 while Currency!="USDT" and Currency!="EUR" and Currency!="GBP":
     Currency = input("Veuillez entrer une monnaie valide (EUR, USDT, GBP) : ")
 
+#Demande à l'utilisateur de choisir un interval de temps pour l'analyse
 Interval = input("Sur combien de temps souhaitez vous analyser ? ")
 while Interval!='1' and Interval!='24':
     Interval = input("Veuillez entrer une intervalle de temps valide pour l'analyse (1 : pour 1heure / 24 : pour 24heure) : ")
 
+#Créer un dictionnaire avec les paramètres renseignés par l'utilisateur
 params = {
     "user" : Username,
     "symbol" : Symbol,
@@ -21,27 +27,36 @@ params = {
     "interval" : Interval
 }
 
+#Fonction qui permet de récupérer les tweets pertinents selon les paramètres
 Author = Get_Relevant_Tweets(params)
 
+#Si le "@" twitter n'existe pas
 if Author==None:
     print("Fin de l'analyse")
+#Si le "twittos" n'a pas tweeté à propos de la cryptomonnaie
 elif Author.Tweets==[]:
     print(Author)
     print("L'utilisateur n'a pas tweeté à propos de "+Symbol)
     print("Fin de l'analyse")
-#Suppression des doublons
 else:
+    #Suppression des doublons
     Author.remove_duplicates()
     print(Author)
 
+    #Trace la courbe historique de la cryptomonnaie (chaque bougie représente 1jour) ainsi que les dates des tweets du "twittos" à propos de la cryptomonnaie
     Author.draw(params)
 
+    #Boucle qui permet de récupérer les données financières de la cryptomonnaie pour chaque tweet sur l'interval de temps choisi au début
+    #Dans un second temps, la boucle calcul l'influence de chaque tweet sur le cours de la cryptomonnaie et son volume de transaction
     for i, tweet in enumerate(Author.Tweets):
         tweet.get_klines(params)
         tweet.get_influence(params)
 
+    #Calcule l'influence moyenne du "twittos" sur le marché de la cryptomonnaie choisie
     Author.get_influence(params)
 
+    #Trace la courbe historique avant/après du tweet qui a eu le plus d'influence à la hausse
+    #Trace également la courbe historique avant/après du tweet qui a eu le plus d'influence à la baisse
     Plot_MostImpactfulTweets(Author, params)
 
     print("")
